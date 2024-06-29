907b9204a3e48819a6ca51e0842d06c5bdf485d8
Ksandr <MYEMAIL>
2024-06-29 16:53:52 +0700

    Added Authentication by cookie (pages with minimal api)

--- Program.cs
+++ Program.cs
@@ -1,25 +1,102 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+        options.Events.OnRedirectToLogin = (context) =>
+        {
+            context.Response.StatusCode = 401;
+            return Task.CompletedTask;
+        });
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
+
+
+// DEL
+var usernames = new List<string>
+{
+    "Admin"
+};
+
+app.MapGet("/login", (string? message, HttpContext context) =>
+{
+    if (context.User.Identity?.IsAuthenticated == true)
+    {
+        return Results.Redirect("/");
+    }
+
+    string formHTML =
+        $"""
+        <p>{message}</p>
+        <form method='post'>
+            <p>
+                <label>Username</label><br />
+                <input name='username' />
+            </p>
+            <input type='submit' value='Login' />
+        </form>
+        """;
+
+    return Results.Content(formHTML, "text/html");
+});
+
+app.MapPost("/login", async (HttpContext context) => 
+{
+    var form = context.Request.Form;
+    string username = form["username"]!;
+    if (!usernames.Contains(username))
+    {
+        string message = $"No user with username = {username}";
+        return Results.Redirect($"/login?message={message}");
+    }
+
+    var claims = new List<Claim>
+    {
+        new Claim(ClaimTypes.Name, username)
+    };
+    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+    var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+    await context.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
+    return Results.Redirect("/");
+});
+
+app.MapGet("/logout", async (HttpContext context) =>
+{
+    await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    return Results.Redirect("/login");
+});
+
+app.Map("/", (HttpContext context) =>
+{
+    string responseHTML = "";
+    if (context.User.Identity?.IsAuthenticated == true)
+    {
+        var username = context.User.FindFirst(ClaimTypes.Name)!.Value;
+        responseHTML =
+        $"""
+        <p>Hello {username}</p>
+        <br>
+        <a href="/logout">Log out<a/>
+        """;
+    }
+    else
+    {
+        responseHTML =
+        """
+        <p>Anonimous users don't have access to this page</p>
+        <p>To get access try to <a href="/login">Log in<a/><p/>
+        """;
+    }
+
+    return Results.Content(responseHTML, "text/html");
+});
 
 app.Run();
